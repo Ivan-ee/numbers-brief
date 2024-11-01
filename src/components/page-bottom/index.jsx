7f4967907297ba67fa72970a77b57739a48bb66e
Ivan-ee <smirnowwwivan@mail.ru>
2024-11-01 14:36:16 +0300

    add mobile version

--- src/components/page-bottom/index.jsx
+++ src/components/page-bottom/index.jsx
@@ -1,8 +1,14 @@
+import {BottomInfo} from "../bottom-info/index.jsx";
+
 export function PageBottom() {
 
     return (
-        <div className="page-bottom">
-            <div className="btn regular_24">Отправить бриф</div>
-        </div>
+        <>
+            <div className="page-bottom">
+                <div className="btn regular_24">Отправить бриф</div>
+            </div>
+            <BottomInfo/>
+        </>
+
     )
 }
